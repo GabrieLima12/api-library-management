86ea005917ddece3471fa1dd7afacbc76cf4c1f9
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-09-18 19:54:19 -0300

    chore: setup project structure and dependencies for API.

---
+++ src/main/java/com/gabriel/api_library_management/ApiLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/api_library_management/ApiLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
